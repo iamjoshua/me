ba8170ee4099b9cc8a8a11dd2afb6e8410cd1faa
Joshua Heiland <joshua@everest.com>
2018-01-30 16:40:26 -0800

    Add mobile styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,9 @@ import styles from "./index.module.scss"
 
 const Header = () => (
   <div className={styles.container}>
-    <div></div>
+    <div>
+      <div></div>
+    </div>
     <div>
       <h1 className={styles.initMargin}>SF-based thinker & <br/> artificial intelligence researcher</h1>
       <h2>designer, coder, writer</h2>
