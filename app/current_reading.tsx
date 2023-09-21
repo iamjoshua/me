6d3f4de72c3e782ae6bf602273fc84d6e0d137c0
Joshua Heiland <thezenman@gmail.com>
2023-09-21 12:48:58 -0600

    style: improve current reading style

--- app/current_reading.tsx
+++ app/current_reading.tsx
@@ -1,30 +1,37 @@
+import Link from "next/link";
+
 interface EventProps {
-  date: string
-  title: string
-  type?: string
-  description?: React.ReactNode
+  date: string;
+  title: string;
+  type?: string;
+  description?: React.ReactNode;
 }
 
 function CurrentReading({ date, title, type, description }: EventProps) {
   return (
-    <div className="p-10 max-h-[300px] overflow-hidden">
-      <div className="flex items-center gap-10">
-        <time className="text-sm font-normal leading-none text-gray-700">
-          {date}
-        </time>
+    <Link href="/philosophy">
+      <div className="px-10 max-h-[300px] overflow-hidden">
         {type && (
-          <span className="py-1 px-2 bg-sky-900 text-white text-[8px] rounded uppercase">
-            {type}
-          </span>
+          <div className="absolute top-5 right-5 xmb-3 flex justify-end">
+            <span className="py-1 px-2 mb-3 bg-sky-900 text-white text-[8px] rounded uppercase">
+              {type}
+            </span>
+          </div>
         )}
-      </div>
-      <h3 className="mb-1 text-lg md:text-base font-semibold text-gray-900 ">{title}</h3>
-      {description && (
-        <p className="mb-4 text-sm font-normal text-gray-700">
-          {description}
-        </p>
-      )}
-      {/* <a
+        <div className="mb-2 flex items-center gap-10">
+          <time className="text-xs font-normal leading-none text-gray-700">
+            {date}
+          </time>
+        </div>
+        <h3 className="mb-1 text-lg md:text-sm font-semibold text-gray-900 ">
+          {title}
+        </h3>
+        {description && false && (
+          <p className="mb-4 text-sm font-normal text-gray-700">
+            {description}
+          </p>
+        )}
+        {/* <a
         href="#"
         className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
       >
@@ -42,8 +49,9 @@ function CurrentReading({ date, title, type, description }: EventProps) {
           ></path>
         </svg>
       </a> */}
-    </div>
-  )
+      </div>
+    </Link>
+  );
 }
 
-export default CurrentReading
+export default CurrentReading;
