e044b55247defa816e8300f1c7e5811cf6723f53
Joshua Heiland <thezenman@gmail.com>
2023-09-21 16:17:56 -0600

    feat: verify github hooks

--- app/heraclitus/route.ts
+++ app/heraclitus/route.ts
@@ -1,16 +1,18 @@
+import * as crypto from "crypto";
 import { revalidateTag } from "next/cache";
+
+const WEBHOOK_SECRET: string = process.env.CACHE_SECRET as string;
 
 export async function POST(request: Request) {
   try {
-    const data = await request.json();
-    console.log("data:", data);
-    const secret = data?.hook?.config?.secret;
-    const isValid = secret === process.env.CACHE_SECRET;
+    const isValid = verifySignature(request);
     if (isValid) {
+      const data = await request.json();
+      console.log("data:", data);
       revalidateTag("essays");
       return new Response("Never the same again\n");
     } else {
-      console.log("secret was wrong:", secret, process.env.CACHE_SECRET);
+      console.log("secret was wrong:");
     }
   } catch (e: any) {
     console.log("JSON parsing failed", e);
@@ -18,3 +20,16 @@ export async function POST(request: Request) {
 
   return new Response("Some things never change\n");
 }
+
+const verifySignature = (req: Request) => {
+  const signature = crypto
+    .createHmac("sha256", WEBHOOK_SECRET)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+  const reqSignature = req.headers.get("x-hub-signature-256") as string;
+
+  const trusted = Buffer.from(`sha256=${signature}`, "ascii");
+  const untrusted = Buffer.from(reqSignature, "ascii");
+
+  return crypto.timingSafeEqual(trusted, untrusted);
+};
