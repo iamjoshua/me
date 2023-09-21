256d6d26c3b252b3aee87d6d1d744ca6fab30116
Joshua Heiland <thezenman@gmail.com>
2023-09-21 17:27:25 -0600

    fix: handle next response object before verifying

--- app/heraclitus/route.ts
+++ app/heraclitus/route.ts
@@ -5,10 +5,7 @@ const WEBHOOK_SECRET: string = process.env.CACHE_SECRET as string;
 
 export async function POST(request: Request) {
   try {
-    const isValid = verifySignature(request);
-    if (isValid) {
-      const data = await request.json();
-      console.log("data:", data);
+    if (await verifySignature(request)) {
       revalidateTag("essays");
       return new Response("Never the same again\n");
     } else {
@@ -21,10 +18,11 @@ export async function POST(request: Request) {
   return new Response("Some things never change\n");
 }
 
-const verifySignature = (req: Request) => {
+const verifySignature = async (req: Request) => {
+  const body = await req.json();
   const signature = crypto
     .createHmac("sha256", WEBHOOK_SECRET)
-    .update(JSON.stringify(req.body))
+    .update(JSON.stringify(body))
     .digest("hex");
   const reqSignature = req.headers.get("x-hub-signature-256") as string;
 
