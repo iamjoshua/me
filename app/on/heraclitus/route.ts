084d9a43499a91eb11c88951ef98a6cd7c7f2108
Joshua Heiland <thezenman@gmail.com>
2024-03-05 09:49:36 -0700

    debugging github request

--- app/on/heraclitus/route.ts
+++ app/on/heraclitus/route.ts
@@ -1,8 +1,19 @@
 import { handleCacheRevalidation } from "@/lib/cacheTools";
 
 export async function POST(request: Request) {
-  console.log("github action");
-  console.log(request);
+  console.log("harry colitus");
+  try {
+    console.log(request?.body);
+  } catch (e) {
+    console.log("first failed");
+  }
+  try {
+    const body = await request.json();
+    console.log("Parsed body");
+    console.log(body);
+  } catch (e) {
+    console.log("parsing failed");
+  }
 
   return await handleCacheRevalidation("essays", request);
 }
